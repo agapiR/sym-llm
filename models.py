bb5860c17255df5fe1d1bd4eaa2e60593b64811d
rissakiagapi <rissakiagapi@vldb.khoury.northeastern.edu>
2023-09-18 23:25:53 -0400

    latest

--- models.py
+++ models.py
@@ -164,15 +164,18 @@ Symbolic Components
 class SymbolicAttentionHead(nn.Module):
     """ one head of symbolic self-attention """
 
-    def __init__(self, cooc, h):
+    def __init__(self, cooc, h, self_loops=True):
         super().__init__()
+        # cooc statistics
         vocab_size,_,n_heads = cooc.shape
         self.h_cooc = cooc[:,:,h]
+        # global representation (Value matrix)
         self.value = nn.Embedding(vocab_size, vocab_size*n_heads)
         symbolic_token_embeddings = torch.flatten(cooc, start_dim=1, end_dim=2) # combine dimensions 1 and 2 into one
         self.value.load_state_dict({'weight': symbolic_token_embeddings})
         self.value.weight.requires_grad = False
-        # print("From SymbolicAttentionHead {}: value embedding\n{}".format(h+1, self.value.weight))
+        # self loop in context-aware representation
+        self.add_self_loops = self_loops
 
     def forward(self, x):
         # input of size (batch, time-step, channels)
@@ -183,11 +186,11 @@ class SymbolicAttentionHead(nn.Module):
         # head size (hs): N*K
         device = x.device
         B,T,C = x.shape
-        # print("From SymbolicAttentionHead-forward pass: #Batches {} #Timesteps {} #Channels {}".format(B,T,C))
         k = x   # (B,T,1)
         q = x   # (B,T,1)
-        att_scores = torch.empty(B, T, T).to(device)
-        # TODO: For now, we just do it per batch. Not the most efficient thing.
+        # Compute the symbolic self-attention scores 
+        att_scores = torch.empty(B, T, T) #.to(device)
+        # TODO: For now, we just do calculate attention scores per batch. Not the most efficient way.
         for b in range(B):
             # compute attention scores ("affinities") for batch
             token_pairs = torch.cartesian_prod(k[b].squeeze(), q[b].squeeze())
@@ -195,25 +198,14 @@ class SymbolicAttentionHead(nn.Module):
             wei = torch.take(self.h_cooc, ind).reshape((T,T)) # (T, T)
             wei = F.softmax(wei, dim=-1).unsqueeze(0) # (1, T, T)
             att_scores[b] = wei
-
-        # ATTENTION SCORES
-        # print("From SymbolicAttentionHead-forward pass: Attention scores\n{}".format(att_scores))
-        # print("From SymbolicAttentionHead-forward pass: Attention scores (shape) {}".format(att_scores.shape))
-        
-        # perform the weighted aggregation of the values
-
-        # VALUES
+            # the representation of the token itself is added, weighted by 1.
+            if self.add_self_loops:
+                self_loops = torch.eye(T, T) #.to(device)
+                att_scores[b] += self_loops
+        # Perform the weighted aggregation of the values
         v = self.value(x.squeeze()) # (B,T,hs)
-        # print("From SymbolicAttentionHead-forward pass: Value embedding\n{}".format(self.value.weight))
-        # print("From SymbolicAttentionHead-forward pass: Input\n{}".format(x.squeeze()))
-        # print("From SymbolicAttentionHead-forward pass: Selected values\n{}".format(v))
-        # print("From SymbolicAttentionHead-forward pass: Selected values (shape) {}".format(v.shape))
-
-        # RE-WRITES
+        # Compute the context-aware representations
         out = att_scores @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
-        # print("From SymbolicAttentionHead-forward pass: Re-write\n{} ".format(out))
-
-        # assert False, "---END of the first forward pass."
         return out
     
 class SymbolicMultiHeadAttention(nn.Module):
@@ -249,6 +241,17 @@ class SymbolicBlock(nn.Module):
         x = x + self.ffwd(self.ln(x))
         return x
 
+class SymbolicMultiHeadAttentionEncoding(nn.Module):
+    """ multi-head symbolic self-attention for encoding, no learned components """
+
+    def __init__(self, cooc, self_loops=True):
+        super().__init__()
+        _,_,num_heads = cooc.shape
+        self.heads = nn.ModuleList([SymbolicAttentionHead(cooc, h, self_loops=self_loops) for h in range(num_heads)])
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1) # (B, T, hs*T)
+        return out
 
 
 """
@@ -257,12 +260,12 @@ Symbolic Transformer
 
 class SymGPTLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size, n_embd, n_head, n_layer, block_size, dropout, device, cooc):
+    def __init__(self, out_vocab_size, n_embd, n_head, n_layer, block_size, dropout, device, cooc):
         super().__init__()
         block_list = [SymbolicBlock(n_embd, cooc, dropout=dropout)] + [Block(n_embd, n_head=n_head, block_size=block_size, dropout=dropout) for _ in range(n_layer-1)]
         self.blocks = nn.Sequential(*block_list)
         self.ln_f = nn.LayerNorm(n_embd) # final layer norm
-        self.lm_head = nn.Linear(n_embd, vocab_size)
+        self.lm_head = nn.Linear(n_embd, out_vocab_size)
 
         # better init, not covered in the original GPT video, but important, will cover in followup video
         self.apply(self._init_weights)
@@ -289,6 +292,119 @@ class SymGPTLanguageModel(nn.Module):
         # pos_emb = self.position_embedding_table(torch.arange(T, device=self.device)) # (T,C)
         # x = tok_emb + pos_emb # (B,T,C)
         x = idx.reshape((B, T, 1)) # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -self.block_size:]
+            # get the predictions
+            logits, _ = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+    
+
+class SymLMEncoder(nn.Module):
+
+    def __init__(self, out_vocab_size, device, cooc, self_loops=True):
+        super().__init__()
+        # symbolic module that produces a contex-aware representation
+        self.sym_attn = SymbolicMultiHeadAttentionEncoding(cooc, self_loops=self_loops)
+        # TODO: implement encoder
+        # encoder module that maps the contex-aware representation into a single symbol
+        # self.encoder = 
+        # store device for forward pass
+        self.device = device
+
+    def forward(self, idx):
+        B, T = idx.shape
+        x = idx.reshape((B, T, 1)) # (B,T,C)
+        # produce context aware intermediate representation
+        x = self.sym_attn(x) # (B,T,C)
+        # focus only on the last time step, 
+        # the last token uses context from the whole context window/block
+        x_rep = x[:, -1, :] # becomes (B, C)
+        # encode the intermediate representation into a new symbol
+        # TODO: encoder not implemented
+        # x_enc = self.encoder(x)
+        x_enc = 0
+        return x_rep, x_enc
+    
+
+
+"""
+Baseline
+"""
+
+class SimpleBlock(nn.Module):
+    """ Symbolic Transformer block: symbolic communication followed by computation """
+
+    def __init__(self, n_embd, dropout=0.5):
+        # n_embd: embedding dimension 
+        super().__init__()
+        self.ffwd = FeedFoward(n_embd, dropout)
+        self.ln = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.ffwd(self.ln(x))
+        return x
+    
+
+class FeedForwardSeqModel(nn.Module):
+    def __init__(self, vocab_size, n_embd, n_head, n_layer, block_size, dropout, device):
+        super().__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        block_list = [SimpleBlock(n_embd, dropout=dropout) for _ in range(n_layer)]
+        self.blocks = nn.Sequential(*block_list)
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self._init_weights)
+
+        # store device for forward pass
+        self.device = device
+
+        # store block size for generation
+        self.block_size = block_size
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=self.device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
         x = self.blocks(x) # (B,T,C)
         x = self.ln_f(x) # (B,T,C)
         logits = self.lm_head(x) # (B,T,vocab_size)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib.pyplot as plt
 
 # data loading
 def get_batch(data, split, block_size, batch_size, device='cpu'):
@@ -10,11 +11,53 @@ def get_batch(data, split, block_size, batch_size, device='cpu'):
     x, y = x.to(device), y.to(device)
     return x, y
 
-def calculate_cooc(sequence, N, K, device='cpu'):
+# data loading
+def get_batch_(data, split, block_size, batch_size, device='cpu'):
+    # generate a small batch of data of inputs x and targets y
+    in_data, out_data = data[split]
+    ix = torch.randint(len(in_data) - block_size, (batch_size,))
+    x = torch.stack([in_data[i:i+block_size] for i in ix])
+    y = torch.stack([out_data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+def calculate_cooc(sequence, N, K, device='cpu', index_dict=None):
     cooc_tensor = torch.zeros((N, N, K))
     for i,d in enumerate(sequence):
+        if index_dict is not None:
+            d = index_dict[d.item()]
         for k in range(K):
             if i+k+1<len(sequence):
                 nd = sequence[i+k+1]
+                if index_dict is not None:
+                    nd = index_dict[nd.item()]
                 cooc_tensor[d,nd,k] += 1
     return cooc_tensor.to(device)
+
+@torch.no_grad()
+def estimate_loss_(model, data_dict, eval_iters, block_size, batch_size, device='cpu'):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch_(data_dict, split, block_size, batch_size, device)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+@torch.no_grad()
+def estimate_loss(model, data_dict, eval_iters, block_size, batch_size, device='cpu'):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(data_dict, split, block_size, batch_size, device)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
